7aae64c7593c86661f634950ee847145fe2e4e2a
Richie Whitcomb <p23rwhitcomb@fairfieldprep.org>
2023-01-20 10:52:29 -0500

    p

---
+++ lab_8-4.py
@@ -0,0 +1,17 @@
+#Creator RMW 12/13/22
+
+def sum_to(x):
+    total = 0
+    for i in range(x+1):
+        total += i
+    return total
+
+def sum_tow(x):
+    total = 0
+    y = 0
+    x = range(x+1)
+    while y <= x:
+        total += y
+    return total
+
+
